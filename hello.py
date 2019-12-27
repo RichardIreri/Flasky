e91186ba897457b3133d2e074c04cac6cd3a7200
RichardIreri <richardmuthoni34@gmail.com>
2019-12-27 14:22:26 +0300

    Templates that uses flask-bootstrap

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 # Rendering templates
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -10,3 +13,8 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.route('/')
+def List():
+    l = [10, 20, 30, 40, 50]
+    for elem in l:
+        return render_template('user.html', comments=l)
