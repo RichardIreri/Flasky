bb2740a72c63fd91069ef4492f5cf20ef3014dca
RichardIreri <richardmuthoni34@gmail.com>
2019-12-30 10:43:02 +0300

    Flask-Moment 	1.Initializing flask moment 	2.Import the Moment.js library 	3.Adding a datetime variable 	4.Timestamps rendering with flask moments

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 # Rendering templates
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+app = Flask(__name__)           # Creating an application instance
+bootstrap = Bootstrap(app)      # Initializing bootstrap
+moment = Moment(app)            # Initializing moment
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
