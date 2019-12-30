8775591cd7bd588ebf7d1c4b4ae7c57eca25313b
RichardIreri <richardmuthoni34@gmail.com>
2019-12-30 12:48:48 +0300

    1.Using flask-wtf and flask-bootstrap to render a form. 2.Handle a web form using GET and POST request method.

--- hello.py
+++ hello.py
@@ -3,14 +3,30 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)           # Creating an application instance
 bootstrap = Bootstrap(app)      # Initializing bootstrap
 moment = Moment(app)            # Initializing moment
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+# Form class definition
+class NameForm(FlaskForm):
+    name = StringField('What is you name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# Handle a web form with GET and POST requests
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data 
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
