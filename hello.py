e2d54d65a64ac9ad78c4c892b41de5b52cba56aa
RichardIreri <richardmuthoni34@gmail.com>
2019-12-31 15:05:31 +0300

    Database management with flask-sqlalchemy: 	1.Database configuration. 	2.Role and user models definition. 	3.Relationships in the database models. 	4.Dynamic database relationships. 	5.Database use in view functions. 	6.Customized greetings in templates.

--- hello.py
+++ hello.py
@@ -6,12 +6,23 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
 
+# Application and extensions initializations
 app = Flask(__name__)           # Creating an application instance
 bootstrap = Bootstrap(app)      # Initializing bootstrap
 moment = Moment(app)            # Initializing moment
+db = SQLAlchemy(app)            # Initializing database
 
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+# Configurations(Flask, Extensions and Application)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 # Form class definition
 class NameForm(FlaskForm):
@@ -23,12 +34,19 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you have changed your name!")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else: # User exists
+            session['known'] = True
         session['name'] = form.name.data 
+        form.name.data = ''
         return redirect(url_for('index'))  # Redirect and user session
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'),
+                            known=session.get('known', False), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -40,4 +58,23 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html'), 500 
+
+# Role and user model definition
+class Role(db.Model):
+    __tablename__='roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+    # String representation for debugging and testing
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    # String representation for debugging and testing
+    def __repr__(self):
+        return '<User %r>' % self.username
