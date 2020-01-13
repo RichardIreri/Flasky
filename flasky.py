80aa3b44dead07b3924502feba7e0daa52d566d0
RichardIreri <richardmuthoni34@gmail.com>
2020-01-13 18:02:29 +0300

    Obtaining code coverage reports.

--- flasky.py
+++ flasky.py
@@ -5,6 +5,14 @@ import os
 from app import create_app, db
 from app.models import User, Role
 from flask_migrate import Migrate
+import sys
+import click
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -21,3 +29,26 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+# Coverage metrics
+@app.cli.command()
+@click.option('--coverage/--no-coverage', default=False,
+                help='Run test under code coverage.')
+def test(coverage):
+    """ Run the unit tests. """
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
