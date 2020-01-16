19984ab49339ecc49d670aad29d2d1cac5175ffc
RichardIreri <richardmuthoni34@gmail.com>
2020-01-16 16:47:18 +0300

    Debugged flasky.py

--- flasky.py
+++ flasky.py
@@ -63,7 +63,7 @@ def profile(length, profile_dir):
         app.run(debug=False)
 
 # Deplpoy command
-@app.cli.command()
+@manager.command
 def deploy():
     """ Run deployment task."""
     # Migrate database to latest vertion.
